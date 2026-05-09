61ae0b84c62f7d2e6429675d1698eb657e885e21
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-05-09 00:41:46 -0300

    upd

--- Aprendendo-Vite-React/src/pages/Home/index.jsx
+++ Aprendendo-Vite-React/src/pages/Home/index.jsx
@@ -1,21 +1,64 @@
-import { useState } from 'react'
-
-import './style.css'
+import { useEffect, useState, useRef } from "react";
+import Trash from "../../assets/lixeira.png";
+import "./style.css";
+import api from '../../services/api.js'
 
 function Home() {
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function getUsers() {
+    const usersFromApi = await api.get('/usuarios')
+    setUsers(usersFromApi.data)
+  } 
+
+  useEffect(() => {
+    getUsers()
+  }, []); 
+
+  async function createUsers() {
+    await api.post('/usuarios', {
+      email: inputEmail.current.value,
+      name: inputName.current.value,
+      age: inputAge.current.value
+    })
+
+    getUsers()
+  } 
+
+    async function deleteUsers(id) {
+      await api.delete(`/usuarios/${id}`)
+      getUsers()
+    } 
 
 
   return (
-    <div className='container'>
-        <form >
-          <h1>Cadastro de Usuários</h1>
-          <input name='nameInput' type='text'/>
-          <input name='ageInput' type='number'/>
-          <input name='emailInput' type='email'/>
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder="Nome" name="nameInput" type="text" ref={inputName} />
+        <input placeholder="Idade" name="ageInput" type="number" ref={inputAge} />
+        <input placeholder="Email" name="emailInput" type="email" ref={inputEmail} />
+        <button type="button" onClick={createUsers}>Cadastrar</button>
+      </form>
 
-        </form>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button onClick={() => deleteUsers(user.id)} >
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
