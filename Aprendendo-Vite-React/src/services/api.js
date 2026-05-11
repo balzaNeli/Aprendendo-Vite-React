ea233f1922f438e42a1cdd00c7768f3526442cdc
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-05-10 22:21:23 -0300

    upd para somente local

--- Aprendendo-Vite-React/src/services/api.js
+++ Aprendendo-Vite-React/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://aprendendo-a-criar-api-production.up.railway.app'
+    baseURL: 'http://localhost:3000/'
 })
 
 export default api
